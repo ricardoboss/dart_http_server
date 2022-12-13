a1ac5d6bb63b211d737cd39f1d87e2b0ab909d67
Ricardo Boss <contact@ricardoboss.de>
2022-12-13 15:15:48 +0100

    Print all headers, make different requests easier to identify

--- bin/dart_http_server.dart
+++ bin/dart_http_server.dart
@@ -4,7 +4,8 @@ void main() async {
   final server = await HttpServer.bind(InternetAddress.anyIPv4, 7000);
   print("Server is running");
   await for (final request in server) {
-    print("uri: ${request.uri}");
+    print("New request: ${request.uri}");
+
     print("scheme: ${request.uri.scheme}");
     print("host: ${request.uri.host}");
     print("port: ${request.uri.port}");
@@ -14,8 +15,13 @@ void main() async {
     try {
       print("requested uri: ${request.requestedUri}");
     } catch (e) {
-      print("requested uri: $e");
+      print("requested uri: ${e.toString().trim()}");
     }
+
+    print("\nHeaders:");
+    print(request.headers);
+
+    print("===================================================");
 
     request.response.close();
   }

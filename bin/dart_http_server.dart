4956740a2d09a3c3739782e972524dc82b7b5b18
Ricardo Boss <contact@ricardoboss.de>
2022-12-13 13:13:26 +0100

    Added more fields

--- bin/dart_http_server.dart
+++ bin/dart_http_server.dart
@@ -4,6 +4,8 @@ void main() async {
   final server = await HttpServer.bind(InternetAddress.anyIPv4, 7000);
   print("Server is running");
   await for (final request in server) {
+    print("uri: ${request.uri}");
+    print("scheme: ${request.uri.scheme}");
     print("host: ${request.uri.host}");
     print("port: ${request.uri.port}");
     print("path: ${request.uri.path}");
